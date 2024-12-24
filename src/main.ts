6014ff117a5cb311bac8debf3ffbffccf42f06e3
Odeh-Momen <MmOdeh@asaltech.com>
2024-12-24 16:55:29 +0200

    project initialization

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Set global prefix for all routes
+  app.setGlobalPrefix('api');
+
+  // Configure Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Asal API Documentation')
+    .setDescription('Backend template for Asal technology')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
