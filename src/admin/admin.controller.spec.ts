c82933d2bf3d4eb1e8d9939c95bea5b287b6c76e
Amjad Omari <amjadomari2008@gmail.com>
2025-01-26 15:12:10 -0800

    unit testing configurations

--- src/admin/admin.controller.spec.ts
+++ src/admin/admin.controller.spec.ts
@@ -1,4 +1,5 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import type { TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
 import { AdminController } from './admin.controller';
 
 describe('AdminController', () => {
